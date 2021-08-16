9fd4a5a028f65980ceae77ace051232151f2004a
Ray Sales <78052891+ray-sales@users.noreply.github.com>
2021-08-16 16:06:57 -0300

    add sum method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Sum();
+        }
+        static void Sum()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float firstValue = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor:");
+            float secondValue = float.Parse(Console.ReadLine());
+
+            float resultado = firstValue + secondValue;
+
+            Console.WriteLine();
+            Console.WriteLine($"O resultado da soma é {resultado}");
         }
     }
+
+
 }
